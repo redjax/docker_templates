383c0972fcf8568381972dbb8481a67f144b1fc3
redjax <jackenyon@gmail.com>
2024-12-03 22:06:27 -0500

    Integrate code from demo.py into main.py
    Remove demo.py

--- templates/games/docker_palworld_server/rpc_mgr/scripts/check_online_players.py
+++ templates/games/docker_palworld_server/rpc_mgr/scripts/check_online_players.py
@@ -6,4 +6,9 @@ from loguru import logger as log
 if __name__ == "__main__":
     setup.setup_logging(log_level="DEBUG", colorize=True, add_error_file_logger=True, add_file_logger=True)
     server_mgr = PalworldRPCController(container_name="palworld-server")
-    server_mgr.shutdown_server(ignore_online=True)
+    online_players = server_mgr.get_players()
+    
+    if online_players:
+        log.info(f"Online players ({len(online_players)}): {online_players}")
+    else:
+        log.info("No players online.")
