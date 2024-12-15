22e2d96aa7860f825a0d7de9b5ffcd5fc0e4658b
redjax <jackenyon@gmail.com>
2024-12-14 21:26:16 -0500

    Committing even though it doesn't work.

--- templates/jupyter/docker_jupyter_hub/jupyterhub/jupyterhub_config.py
+++ templates/jupyter/docker_jupyter_hub/jupyterhub/jupyterhub_config.py
@@ -1,61 +1,60 @@
-# Copyright (c) Jupyter Development Team.
-# Distributed under the terms of the Modified BSD License.
+# JupyterHub configuration
+#
+## If you update this file, do not forget to delete the `jupyterhub_data` volume before restarting the jupyterhub service:
+##
+##     docker volume rm jupyterhub_jupyterhub_data
+##
+## or, if you changed the COMPOSE_PROJECT_NAME to <name>:
+##
+##    docker volume rm <name>_jupyterhub_data
+##
 
-# Configuration file for JupyterHub
 import os
 
-c = get_config()  # noqa: F821
+## Generic
+c.JupyterHub.admin_access = True
+c.Spawner.default_url = '/lab'
 
-# We rely on environment variables to configure JupyterHub so that we
-# avoid having to rebuild the JupyterHub container every time we change a
-# configuration parameter.
+## Authenticator
+from jhub_cas_authenticator.cas_auth import CASAuthenticator
+c.JupyterHub.authenticator_class = CASAuthenticator
 
-# Spawn single-user servers as Docker containers
-c.JupyterHub.spawner_class = "dockerspawner.DockerSpawner"
+# The CAS URLs to redirect (un)authenticated users to.
+c.CASAuthenticator.cas_login_url = 'https://cas.uvsq.fr/login'
+c.CASLocalAuthenticator.cas_logout_url = 'https://cas.uvsq/logout'
 
-# Spawn containers from this image
-c.DockerSpawner.image = os.environ["DOCKER_NOTEBOOK_IMAGE"]
+# The CAS endpoint for validating service tickets.
+c.CASAuthenticator.cas_service_validate_url = 'https://cas.uvsq.fr/serviceValidate'
 
-# Connect containers to this Docker network
-network_name = os.environ["DOCKER_NETWORK_NAME"]
-c.DockerSpawner.use_internal_ip = True
-c.DockerSpawner.network_name = network_name
+# The service URL the CAS server will redirect the browser back to on successful authentication.
+c.CASAuthenticator.cas_service_url = 'https://%s/hub/login' % os.environ['HOST']
 
-# Explicitly set notebook directory because we'll be mounting a volume to it.
-# Most `jupyter/docker-stacks` *-notebook images run the Notebook server as
-# user `jovyan`, and set the notebook directory to `/home/jovyan/work`.
-# We follow the same convention.
-notebook_dir = os.environ.get("DOCKER_NOTEBOOK_DIR", "/home/jovyan/work")
+c.Authenticator.admin_users = { 'lucadefe' }
+
+
+## Docker spawner
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
+c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
+# See https://github.com/jupyterhub/dockerspawner/blob/master/examples/oauth/jupyterhub_config.py
+c.JupyterHub.hub_ip = os.environ['HUB_IP']
+
+# user data persistence
+# see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
+notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan'
 c.DockerSpawner.notebook_dir = notebook_dir
+c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
 
-# Mount the real user's Docker volume on the host to the notebook user's
-# notebook directory in the container
-c.DockerSpawner.volumes = {"jupyterhub-user-{username}": notebook_dir}
+# Other stuff
+c.Spawner.cpu_limit = 1
+c.Spawner.mem_limit = '10G'
 
-# Remove containers once they are stopped
-c.DockerSpawner.remove = True
 
-# For debugging arguments passed to spawned containers
-c.DockerSpawner.debug = True
-
-# User containers will access hub by container name on the Docker network
-c.JupyterHub.hub_ip = "jupyterhub"
-c.JupyterHub.hub_port = 8080
-
-# Persist hub data on volume mounted inside container
-c.JupyterHub.cookie_secret_file = "/data/jupyterhub_cookie_secret"
-c.JupyterHub.db_url = "sqlite:////data/jupyterhub.sqlite"
-
-# Allow all signed-up users to login
-c.Authenticator.allow_all = True
-
-# Authenticate users with Native Authenticator
-c.JupyterHub.authenticator_class = "nativeauthenticator.NativeAuthenticator"
-
-# Allow anyone to sign-up without approval
-c.NativeAuthenticator.open_signup = True
-
-# Allowed admins
-admin = os.environ.get("JUPYTERHUB_ADMIN")
-if admin:
-    c.Authenticator.admin_users = [admin]
+## Services
+c.JupyterHub.services = [
+    {
+        'name': 'cull_idle',
+        'admin': True,
+        'command': 'python /srv/jupyterhub/cull_idle_servers.py --timeout=3600'.split(),
+    },
+]
