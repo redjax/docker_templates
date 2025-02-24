b818ce4fc184509a4bfe07c38f8141c96a4f5127
redjax <no@none.com>
2025-02-24 12:53:52 -0500

    Update count_templates script.
    Preview JSON before saving when debugging.
    
    Preview CSV data before saving when debugging.

--- scripts/count_templates.py
+++ scripts/count_templates.py
@@ -257,9 +257,17 @@ def discover_templates(
 def save_templates_to_json(
     templates: list[dict[str, t.Union[str, Path]]], json_file: str
 ):
+    if not templates:
+        log.warning("No data to save as JSON")
+        return
+
+    log.info(f"Saving templates to JSON file: {json_file}")
+
     Path(json_file).parent.mkdir(parents=True, exist_ok=True)
     with open(json_file, "w", encoding="utf-8") as f:
-        json.dump(templates, f, indent=4, default=str, sort_keys=True)
+        json_data = json.dumps(templates, indent=4, default=str, sort_keys=True)
+        log.debug(f"JSON data:\n{json_data}")
+        f.write(json_data)
     log.info(f"Saved JSON: {json_file}")
 
 
@@ -269,6 +277,9 @@ def save_templates_to_csv(
     if not templates:
         log.warning("No data to save as CSV")
         return
+
+    log.info(f"Saving templates to CSV file: {csv_file}")
+
     Path(csv_file).parent.mkdir(parents=True, exist_ok=True)
     with open(csv_file, "w", newline="", encoding="utf-8") as f:
         writer = csv.DictWriter(f, fieldnames=templates[0].keys())
