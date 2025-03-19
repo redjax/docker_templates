93d50ec4349e56f69261bca2ddc69ec64e5b9e5c
redjax <no@none.com>
2025-03-19 15:30:03 +0000

    Add ignore_in_count patterns
    Update beacons, constants.
    
    Add count script to metadata package

--- scripts/metadata/constants.py
+++ scripts/metadata/constants.py
@@ -6,12 +6,17 @@ __all__ = [
     "TEMPLATE_INDICATORS",
     "TEMPLATE_BEACONS",
     "CATEGORIES_METADATA_FILE",
+    "METADATA_DIR",
+    "IGNORE_COUNT_PATTERNS_FILE"
 ]
 
 TEMPLATES_ROOT: str = "templates"
 JINJA_TEMPLATE_DIR: str = "map/_template"
 OUTPUT_DIR: str = "map"
-IGNORE_CATEGORY_NAMES_FILE: str = "metadata/ignore_categories"
+METADATA_DIR: str = "metadata"
+IGNORE_CATEGORY_NAMES_FILE: str = f"{METADATA_DIR}/ignore_categories"
 TEMPLATE_INDICATORS: str = ["*.env", "compose.yml", "docker-compose.yml", "*.env.example"]
 TEMPLATE_BEACONS: dict = {"category": ".category", "docker_template": ".docker-compose.template", "cookiecutter_template": ".cookiecutter.template"}
-CATEGORIES_METADATA_FILE: str = "metadata/categories.json"
+CATEGORIES_METADATA_FILE: str = f"{METADATA_DIR}/categories.json"
+## Directory/file patterns to ignore during discovery
+IGNORE_COUNT_PATTERNS_FILE: list[str] = f"{METADATA_DIR}/ignore_in_count"
