4a8123c2be1930d27f6c998289c53cbaa7190a03
redjax <no@none.com>
2025-03-20 03:07:38 +0000

    Start work on unified CLI for repo operations.
    Add entrypoints in __main__.py, main.py, and a cli.py at the repo root

---
+++ scripts/metadata/domain/beacon/_dataclasses.py
@@ -0,0 +1,19 @@
+import logging
+from dataclasses import dataclass, field
+import typing as t
+from pathlib import Path
+
+log = logging.getLogger(__name__)
+
+__all__ = ["BeaconFile"]
+
+@dataclass
+class BeaconFile():
+    friendly_name: str
+    beacon: str
+    beacon_path: str
+    
+    @property
+    def path(self) -> Path:
+        return Path(str(self.beacon_path)).expanduser() if "~" in self.beacon else Path(str(self.beacon_path))    
+    
