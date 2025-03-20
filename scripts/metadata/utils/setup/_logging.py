1768fcbf4cfcc0b629fa6f9e963c4433df2a9f7b
redjax <no@none.com>
2025-03-20 03:07:38 +0000

    Start work on unified CLI for repo operations.
    Add entrypoints in __main__.py, main.py, and a cli.py at the repo root

---
+++ scripts/metadata/utils/setup/_logging.py
@@ -0,0 +1,17 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+__all__ = ["DEFAULT_LOGGING_FMT", "DEBUG_LOGGING_FMT", "DEFAULT_DATE_FMT", "setup_logging"]
+
+DEFAULT_LOGGING_FMT: str = "%(asctime)s | %(levelname)s |> %(message)s"
+DEBUG_LOGGING_FMT: str = "%(asctime)s | %(levelname)s |  |> %(message)s"
+DEFAULT_DATE_FMT: str = "%Y-%m-%d %H:%M:%S"
+
+
+def setup_logging(level: str = "INFO", fmt: str = DEFAULT_LOGGING_FMT, datefmt: str = DEFAULT_DATE_FMT):
+    logging.basicConfig(
+        level=level,
+        format=fmt,
+        datefmt=datefmt,
+    )
