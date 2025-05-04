ba7b55367220fca0ef9882f1903ae8d729b6fee3
redjax <no@none.com>
2025-05-03 22:17:43 -0400

    Add comment to ufw allow script

--- templates/games/docker_vrising-server/scripts/ufw_allow_ports.sh
+++ templates/games/docker_vrising-server/scripts/ufw_allow_ports.sh
@@ -2,6 +2,11 @@
 
 set -eo pipefail
 
+## If you set a different port for your V Rising server in the .env file,
+#  change these before running the script
+GAME_PORT=9876
+QUERY_PORT=9877
+
 if [ "$EUID" -ne 0 ]
   then echo "Script requires elevated permissions, please re-run with sudo"
   exit 2
@@ -11,9 +16,6 @@ if ! command -v ufw &> /dev/null; then
     echo "ufw is not installed, exiting.."
     exit 1
 fi
-
-GAME_PORT=9876
-QUERY_PORT=9877
 
 echo "Allowing V Rising ports $GAME_PORT and $QUERY_PORT"
 
