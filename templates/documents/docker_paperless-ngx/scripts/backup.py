6f4635d68b0215cd131d683dab0db5c8e29e9264
redjax <no@none.com>
2025-06-07 20:49:03 -0400

    Check for docker, compose installed

--- templates/documents/docker_paperless-ngx/scripts/backup.py
+++ templates/documents/docker_paperless-ngx/scripts/backup.py
@@ -14,6 +14,33 @@ def parse_args():
     return args
 
 
+def check_docker_installed():
+    try:
+        result = subprocess.run(
+            ["docker", "--version"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        
+        return result.returncode == 0
+    except FileNotFoundError:
+        return False
+
+
+def check_compose_installed():
+    try:
+        result = subprocess.run(
+            ["docker", "compose", "version"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        if result.returncode == 0:
+            return True
+    except FileExistsError:
+        return False    
+
+
 def main(args: argparse.Namespace):
     log_level: str = "DEBUG" if args.debug else "INFO"
     log_fmt: str = "%(asctime)s | %(levelname)s | %(module)s.%(funcName)s:%(lineno)s :: %(message)s" if args.debug else "%(asctime)s | %(levelname)s :: %(message)s"
@@ -21,6 +48,25 @@ def main(args: argparse.Namespace):
     
     logging.basicConfig(level=log_level, format=log_fmt, datefmt=log_datefmt)
     log.debug("DEBUG logging enabled")
+    
+    docker_installed = check_docker_installed()
+    compose_installed = check_compose_installed()
+    
+    log.debug(f"Found Docker: {docker_installed}")
+    log.debug(f"Found Docker Compose: {compose_installed}")
+    
+    if docker_installed and compose_installed:
+        log.info("Found Docker and Docker Compose")
+    elif docker_installed and not compose_installed:
+        log.error("Found Docker, but Docker Compose is not installed")
+        exit(1)
+    elif not docker_installed and compose_installed:
+        log.error("Docker is not installed, but found Doccker Compose")
+        exit(1)
+    else:
+        log.error("Could not find Docker or Docker Compose")
+        exit(1)
+
 
 if __name__ == "__main__":
     args = parse_args()
