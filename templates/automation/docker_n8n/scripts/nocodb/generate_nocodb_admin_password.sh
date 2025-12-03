3bf610e3e002c3ab673e172db4edf72b348e83c1
redjax <no@none.com>
2025-12-03 00:12:52 -0500

    nocodb: Add nocodb + postgres container
    Update .env file
    
    Add scripts for setting up nocodb

---
+++ templates/automation/docker_n8n/scripts/nocodb/generate_nocodb_admin_password.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function generate_nocodb_admin_password() {
+    if ! command -v openssl &>/dev/null; then
+        echo "[ERROR] openssl is not installed"
+        exit 1
+    fi
+
+    echo "NocoDB admin password:"
+    openssl rand -base64 12 | tr -d "=+/" | cut -c1-16
+}
+
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    generate_nocodb_admin_password
+fi
