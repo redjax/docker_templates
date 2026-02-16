f569eeb59f86d7add738a19bed87e4770d629a54
redjax <5534031+redjax@users.noreply.github.com>
2026-02-16 11:03:35 -0500

    radicale: Use safe paths, cd and trap on exit

--- templates/dav/docker_radicale/scripts/backup-radicale.sh
+++ templates/dav/docker_radicale/scripts/backup-radicale.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-BACKUP_DIR=./backups
+THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT="$(realpath -m "$THIS_DIR/..")"
+
+BACKUP_DIR="./backups"
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 ## The compose.yml has `name: radicale`, and the volume's
 #  name is `radicale_data`. This becomes `radicale_radicale_data`
 VOLUME_NAME=radicale_radicale_data
 
+CWD=$(pwd)
+
+function cleanup() {
+  cd "$CWD"
+}
+trap cleanup EXIT
+
+cd "${PROJECT_ROOT}"
 mkdir -p "$BACKUP_DIR"
 
 docker run --rm \
