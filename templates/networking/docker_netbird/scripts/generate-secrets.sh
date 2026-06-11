0c0a930e5f34e8f7a990b1ed0a6b2f89db510a5c
redjax <redjax@users.noreply.github.com>
2026-06-11 10:18:14 -0400

    Feat/netbird (#380)
    * feat(netbird): Start work on netbird template
    
    * feat(netbird): Start work on netbird template
    
    * feat(netbird): Start work on netbird template
    
    * netbird: Stuff runs but nothing works.
    
    * refactor: Move existing netbird templates into .archive dir
    
    The template didn't work, I'm tearing it down and starting over
    
    * gitignore: Add ignore rules
    
    * gitignore: Add netbird config, dashboard ignores
    
    * direnv: Add .envrc for direnv
    
    * scripts: Add netbird scripts
    
    * feat(netbird): Add initial netbird container stack
    
    Add compose file, caddyfile, netbird config template, example .env, and
    postgres layer.
    
    Untested.
    
    * fix(caddy): Remove caddyfile
    
    Move caddyfile to a template rendered with the render-configs.sh script.
    
    This keeps the Caddyfile generic so I don't have to embed my domain
    name.
    
    * script(auth): Fix openssl auth generators
    
    Use -base64 and trim secrets.
    
    * ignore: Add caddyfile to gitignores
    
    * script(render): Add caddyfile rendering
    
    * fix(oauth): Handle Caddy oauth2 route
    
    * script(generate): Update secret generator
    
    Fix auth secret format.
    
    Fix store encryption key length.
    
    * fix(caddy): Add letsencrypt email var
    
    * fix(caddy): Fix routes
    
    Use handle instead of handle_path.
    
    * fix(datacollection): Add param to control data collection, default disabled
    
    * docs(netbird): Add netbird readme docs
    
    * cleanup: Delete old archived version of netbird
    
    * fix(dashboard): Add dashboard env template
    
    * fix(templates): Move templated files into templates/ dir
    
    * fix(templates): Fix template path in script
    
    * docs(netbird): Add requirements section
    
    * docs(netbird): Add links section
    
    * docs(toc): Add table of contents
    
    * docs(note): Add assumptions note
    
    ---------
    
    Co-authored-by: redjax <no@none.com>
    Co-authored-by: redjax <5534031+redjax@users.noreply.github.com>

---
+++ templates/networking/docker_netbird/scripts/generate-secrets.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if ! command -v openssl >&/dev/null; then
+  echo "[ERROR] OpenSSL is not installed" >&2
+  exit 1
+fi
+
+echo "Generating NetBird secrets"
+echo
+
+## NetBird core secrets
+NETBIRD_AUTH_SECRET=$(openssl rand -hex 32 | tr -d '[:space:]')
+NETBIRD_STORE_ENCRYPTION_KEY=$(openssl rand 32 | base64 | tr -d '\n')
+
+## Optional Postgres secret
+POSTGRES_PASSWORD=$(openssl rand -hex 24)
+
+## Output rendered secrets
+
+cat <<EOF
+NETBIRD_AUTH_SECRET=${NETBIRD_AUTH_SECRET}
+NETBIRD_STORE_ENCRYPTION_KEY=${NETBIRD_STORE_ENCRYPTION_KEY}
+POSTGRES_PASSWORD=${POSTGRES_PASSWORD}
+
+NOTE: You only need to use the POSTGRES_PASSWORD if you're using the overlays/postgres.yml layer
+
+Run the following to export the variables to your environment if you are running ./scripts/render-config.sh:
+
+export NETBIRD_AUTH_SECRET=${NETBIRD_AUTH_SECRET}
+export NETBIRD_STORE_ENCRYPTION_KEY=${NETBIRD_STORE_ENCRYPTION_KEY}
+## Optional
+export POSTGRES_PASSWORD=${POSTGRES_PASSWORD}
+EOF
+
+echo
+echo "[OK] Secrets generated"
+
