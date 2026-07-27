051ce01aa392f2781a75b8d84de4739c085bab6c
redjax <5534031+redjax@users.noreply.github.com>
2026-07-27 11:34:52 -0400

    script: Fix migrations script
    Sets permissions on directory paths after doing migrations.

--- templates/dav/docker_davis/sqlite_version/first-launch-migrations.sh
+++ templates/dav/docker_davis/sqlite_version/first-launch-migrations.sh
@@ -9,6 +9,13 @@ echo ""
 echo "Running Davis migrations."
 echo ""
 
-docker compose run --rm davis \
-  sh -c "mkdir -p /data /data/var && chown -R 82:82 /data && bin/console doctrine:migrations:migrate --no-interaction"
+# docker compose run --rm davis \
+#   sh -c "mkdir -p /data /data/var && chown -R 82:82 /data && bin/console doctrine:migrations:migrate --no-interaction"
 
+docker compose run --rm davis sh -c '
+  mkdir -p /data /data/var &&
+  mkdir -p /webdav/public /webdav/homes /tmp &&
+  chown -R 82:82 /data /webdav &&
+  bin/console doctrine:migrations:migrate --no-interaction &&
+  chown -R 82:82 /data /webdav
+'
